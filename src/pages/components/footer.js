4177ca23473d799315528ccbd12291bdf5a1d494
olganedelcu <olganedelcuam@gmail.com>
2020-05-22 20:34:25 +0200

    added components folder and footer

---
+++ src/pages/components/footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Created by Olga Nedelcu,  © 2020</p>
+        </footer>
+    )
+}
+export default Footer
