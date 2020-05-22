bbee5c0bb562cb991b11221a15a4a54490ada4d2
olganedelcu <olganedelcuam@gmail.com>
2020-05-22 21:29:40 +0200

    made it easy to access footer and header

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
+import Header from './header'
+import Footer from './footer'
+
+
 const Layout = () => {
     return (
         <div>
+            <Header />
             {props.children}
+            <Footer />
         </div>
     )
 }
