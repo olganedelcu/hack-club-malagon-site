24c2d198a0b108291e806c2be3ed0de4b7a199d4
olganedelcu <olganedelcuam@gmail.com>
2020-05-22 20:37:54 +0200

    just added the footer component

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,9 +4,11 @@ import Footer from '../pages/components/footer'
 const ContactMe = () => {
     return (
         <div>
+        <p>Don't be shy and contact me!!💩</p>
          <a href="https://github.com/olganedelcu" target="_black">github</a><br />
          <a href="https://www.linkedin.com/in/olga-nedelcu-919243193/" target="_black">linkedin</a><br />
          <a href="https://dev.to/olganedelcu" target="_black">dev</a>
+        <Footer />
         </div>
     )
 }
