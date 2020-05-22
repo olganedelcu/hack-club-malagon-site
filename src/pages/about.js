eed99c6088d4e8cf3f544c6e6086daa36063f5bc
olganedelcu <olganedelcuam@gmail.com>
2020-05-22 19:25:51 +0200

    added some new files

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Title = () => {
+    return (
+        <div>
+            <h1>About me</h1>
+            <p>Like the lyrics let's talk about me baby!</p>
+        </div>
+    )
+}
+
+export default Title 
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,7 +3,9 @@ import React from 'react'
 const BlogContent = () => {
     return (
         <div>
-            <h1></h1>
+            <h1>Blog</h1>
+            <p>Post will be shou up here later on.</p>
         </div>
     )
 }
+export default BlogContent
