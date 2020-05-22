f3c3712b10bd8ce36f8585b2214d517575f3b9f8
olganedelcu <olganedelcuam@gmail.com>
2020-05-22 20:08:57 +0200

    added some links to contact page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
+import { Link } from 'gatsby'
 
 const IndexPage = () => {
   return (
     <div>
       <h1>Hello,</h1>
       <p>I'm Olga Nedelcu</p>
+      <Link to="./contact.js">Contact me!</Link>
     </div>
   )
 }
