b441c2304dfae51ea44796593b066f23f522d05a
olganedelcu <olganedelcuam@gmail.com>
2020-05-25 20:18:51 +0200

    solving some bugs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,6 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [
-    `gatsby-plugin-sass`
-  ],
+  plugins:[
+      'gatsby-plugin-sass'
+  ]
 }
+
