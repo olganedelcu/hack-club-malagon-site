f220b9cb502479525fa4ad10039fde81559ae75f
olganedelcu <olganedelcuam@gmail.com>
2020-05-25 19:39:36 +0200

    deleted soan

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,7 +7,7 @@ const ContactMe = () => {
     return (
         <div>
             <Layout>
-                <h1><span>💥</span> Don't be shy and contact me!!  <span> 💥 😍</span></h1>
+                <h1>💥 Don't be shy and contact me!! 💥 😍</h1>
                 <a href="https://github.com/olganedelcu" target="_black">github</a><br />
                 <a href="https://www.linkedin.com/in/olga-nedelcu-919243193/" target="_black">linkedin</a><br />
                 <a href="https://dev.to/olganedelcu" target="_black">dev</a>
