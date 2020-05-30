8f2ddb085e9e62582b88531156a90bbbba6e310e
olganedelcu <olganedelcuam@gmail.com>
2020-05-30 22:40:58 +0200

    trying to generate a slug for each post

---
+++ gatsby-node.js
@@ -0,0 +1,10 @@
+module.exports.onCreateNode = ({ node, actions }) => {
+    const { createNodeField } = actions
+
+
+        if (node.internal.type === 'MarkdownRemark'){
+            console.log(JSON.stringify(node, undefined, 4))
+        }
+    
+    
+}
