e177abf891ddce1be49358c9781c9343fd326719
olganedelcu <olganedelcuam@gmail.com>
2020-05-30 21:09:37 +0200

    added dinamic query for footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
 
 const Footer = () => {
+    const data = useStaticQuery(graphql`
+    query {
+        site{
+            siteMetadata {
+                author
+            }
+        }
+    }
+    `)
     return (
         <div>
-            <p>Created by Olga Nedelcu,  © 2020</p>
+            <p>Created by {data.site.siteMetadata.author},  © 2020</p>
         </div>
     )
 }
