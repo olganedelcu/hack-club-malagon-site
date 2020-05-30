c022c5ad2e547c8762f7c1d21c7cd497e2d9dee2
olganedelcu <olganedelcuam@gmail.com>
2020-05-30 20:33:21 +0200

    added an active Cass Name

--- src/components/header.js
+++ src/components/header.js
@@ -12,16 +12,16 @@ const Header = () => {
                 <nav>
                     <ul className={headerStyles.navList}>
                         <li>
-                            <Link to="/about" className={headerStyles.navItem} >About</Link>
+                            <Link to="/about" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>About</Link>
                         </li>
                         <li>
-                            <Link to="/index" className={headerStyles.navItem}>Home</Link>
+                            <Link to="/index" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>Home</Link>
                         </li>
                         <li>
-                            <Link to="/blog" className={headerStyles.navItem}>Blog</Link>
+                            <Link to="/blog" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>Blog</Link>
                         </li>
                         <li>
-                            <Link to="/contact" className={headerStyles.navItem}>Contact</Link>
+                            <Link to="/contact" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>Contact</Link>
                         </li>
                     </ul>
                 </nav>
