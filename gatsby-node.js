45b32acd5fcdbb2eb4cabc4d530840e1b8e7b11e
olganedelcu <olganedelcuam@gmail.com>
2020-05-30 22:47:29 +0200

    generated slug for each post

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,17 @@
+const path = require('path')
+
 module.exports.onCreateNode = ({ node, actions }) => {
     const { createNodeField } = actions
 
 
         if (node.internal.type === 'MarkdownRemark'){
-            console.log(JSON.stringify(node, undefined, 4))
+            const slug = path.basename(node.fileAbsolutePath, '.md')
+
+            createNodeField({
+                node,
+                name: 'slug',
+                value: slug
+            })
         }
     
     
