75f1c9f4a403a69b28edbea0d060280a690c288a
olganedelcu <olganedelcuam@gmail.com>
2020-05-31 18:18:21 +0200

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,10 @@ import Layout from '../components/layout'
 
 const NotFound = () => {
     return(
-
+        <Layout>
+            <h1>Page not found</h1>
+            <p><Link to="/">Head home</Link></p>
+        </Layout>
     )
 }
 
