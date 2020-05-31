03b7c68f3a70fcbe06ab32d7d783c3106f4e7251
olganedelcu <olganedelcuam@gmail.com>
2020-05-31 18:27:38 +0200

    just adding the helmet component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from "react"
 import { Link } from 'gatsby'
 
+import Head from '../components/head'
 import Layout from'../components/layout'
 
 const IndexPage = () => {
   return (
     <div>
       <Layout>
+        <Head />
       <h1>Hello. </h1>
       <p>This is the official website of Hack Club Malagón</p>
     </Layout>
