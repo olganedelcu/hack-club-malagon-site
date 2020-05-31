33c6b2268801a7f79168405b5052509ccff62215
olganedelcu <olganedelcuam@gmail.com>
2020-05-31 19:29:50 +0200

    Developing the Home site

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,9 +9,13 @@ const IndexPage = () => {
     <div>
       <Layout>
       <Head title="Home"/>
-      <h1>Hello. </h1>
-      <p>This is the official website of Hack Club Malagón</p>
-      <p>Hey there!</p>
+      <h1>What is Hack Club?</h1>
+      <p>Hack Club is a global network of programming clubs where members learn to code through tinkering and building projects.</p>
+      <h2>What's Hack Club Malagón?</h2>
+      
+      
+      <p>Nowadays learning to program is like getting a super power‍🚀: switched from consumer to creator.<br /> Suddenly, computers become a tool for creating.
+      You have never had a better time to do it; Anywhere in the world, anyone with a laptop and wifi can learn to create an application. Building things has never been so globally democratized!</p>
     </Layout>
     </div>  
   )
