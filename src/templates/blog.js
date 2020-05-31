f5861dfe64df8082f21de139e46f3b4fd7fc741c
olganedelcu <olganedelcuam@gmail.com>
2020-05-31 13:08:41 +0200

    added a blog template

---
+++ src/templates/blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const Blog = () => {
+    return (
+        <Layout>
+            This is the blog template
+        </Layout>
+    )
+}
+
+return default Blog
