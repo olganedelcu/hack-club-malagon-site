979a7e9c194f23aa915dd9f27077fb5293fac639
olganedelcu <olganedelcuam@gmail.com>
2020-05-31 14:42:54 +0200

    dinamic slug

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -3,11 +3,32 @@ import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 
-const Blog = () => {
+
+export const query = graphql`
+query (
+    $slug: String!
+  ) { 
+    markdownRemark (
+      fields: {
+        slug: {
+          eq: $slug
+        }
+      }
+    ) {
+      frontmatter {
+        title 
+        date
+      }
+      html
+    }
+  }
+`
+
+const Blog = (props) => {
     return (
         <div>
         <Layout>
-            This is the blog template
+            <h1>{props.data.markdownRemark.frontmatter.title}</h1>
         </Layout>
         </div>
     )
