f586a07d142042d81bbe1962f96ecfb3e509d72b
olganedelcu <olganedelcuam@gmail.com>
2020-05-31 17:06:44 +0200

    added more content

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,33 +1,18 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 
 import Layout from '../components/layout'
 
-// export const query = graphql`
-// query (
-//     $slug: String!
-//   ) { 
-//     markdownRemark (
-//      fields: {
-//        slug: {
-//          eq: $slug
-//        }
-//      }
-//    ) {
-//      frontmatter {
-//         title 
-//        date
-//      }
-//       html
-//    }
-//  }
-// `
 
 export const query = graphql`
   query($slug: String!){
     contentfulBlogPost(slug: {eq: $slug}) {
       title
       publishedDate(formatString: "MMMM Do, YYYY")
+      body {
+        json
+      }
     }
   }
 `
@@ -38,6 +23,7 @@ const Blog = (props) => {
       <Layout>
        <h1>{props.data.contentfulBlogPost.title}</h1>
        <p>{props.data.contentfulBlogPost.publishedDate}</p>
+        {documentToReactComponents(props.data.contentfulBlogPost.body.json)}
       </Layout>
     </div>
   )
