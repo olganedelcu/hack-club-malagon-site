1dcba7807c2a0a71e42c8c2b022c7e4a0ebb0ee3
olganedelcu <olganedelcuam@gmail.com>
2020-05-31 15:27:23 +0200

    styles posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -29,7 +29,7 @@ const BlogContent = () => {
             <ol className={blogStyles.posts}>
                 {data.allMarkdownRemark.edges.map((edge) => {
                     return (
-                        <li>
+                        <li className={blogStyles.post}>
                             <Link to={`/blog/${edge.node.fields.slug}`}>
                             <h2>{edge.node.frontmatter.title} </h2>
                             <p>{edge.node.frontmatter.date} </p>    
