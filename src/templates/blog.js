83c39b9f31b9e5edce31e341b54b6a8bd921bd6b
olganedelcu <olganedelcuam@gmail.com>
2020-05-31 18:47:08 +0200

    dinamic Helmet for blog posts

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -3,6 +3,7 @@ import { graphql } from 'gatsby'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 
 import Layout from '../components/layout'
+import Head from '../components/head'
 
 
 export const query = graphql`
@@ -31,6 +32,7 @@ const Blog = (props) => {
   return (
     <div>
       <Layout>
+        <Head title={props.data.contentfulBlogPost.title} />
        <h1>{props.data.contentfulBlogPost.title}</h1>
        <p>{props.data.contentfulBlogPost.publishedDate}</p>
         {documentToReactComponents(props.data.contentfulBlogPost.body.json, options)}
