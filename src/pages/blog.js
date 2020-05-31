7fa4e84dfdeb6ddfae84d00d9b1e61173a624cff
olganedelcu <olganedelcuam@gmail.com>
2020-05-31 16:07:13 +0200

    rendering new data

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,37 +7,33 @@ import blogStyles from './blog.module.scss'
 const BlogContent = () => {
     const data = useStaticQuery(graphql`
     query {
-        allMarkdownRemark {
+        allContentfulBlogPost (sort: {fields: publishedDate ,order:DESC}) {
             edges {
-                node {
-                    frontmatter{
-                        title
-                        date
-                    }
-                    fields {
-                        slug
-                    }
+              node {
+                title
+                slug
+                publishedDate(formatString:"MMMM Do, YYYY")
+                     }
                 }
-            }
-        }
+          }
     }
     `)
     return (
         <div>
             <Layout>
-            <h1>Blog</h1>
-            <ol className={blogStyles.posts}>
-                {data.allMarkdownRemark.edges.map((edge) => {
-                    return (
-                        <li className={blogStyles.post}>
-                            <Link to={`/blog/${edge.node.fields.slug}`}>
-                            <h2>{edge.node.frontmatter.title} </h2>
-                            <p>{edge.node.frontmatter.date} </p>    
-                            </Link>
-                        </li>
-                    )
-                })}
-            </ol>
+                <h1>Blog</h1>
+                <ol className={blogStyles.posts}>
+                    {data.allMarkdownRemark.edges.map((edge) => {
+                        return (
+                            <li className={blogStyles.post}>
+                                <Link to={`/blog/${edge.node.fields.slug}`}>
+                                    <h2>{edge.node.frontmatter.title} </h2>
+                                    <p>{edge.node.frontmatter.date} </p>
+                                </Link>
+                            </li>
+                        )
+                    })}
+                </ol>
             </Layout>
         </div>
     )
