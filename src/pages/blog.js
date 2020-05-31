f982330d526b7304a358bb75cd2b20b081626a74
olganedelcu <olganedelcuam@gmail.com>
2020-05-31 15:20:47 +0200

    deleted ugly numbers with css

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link, graphql, useStaticQuery } from 'gatsby'
 
 import Layout from '../components/layout'
-
+import blogStyles from './blog.module.scss'
 
 const BlogContent = () => {
     const data = useStaticQuery(graphql`
@@ -26,7 +26,7 @@ const BlogContent = () => {
         <div>
             <Layout>
             <h1>Blog</h1>
-            <ol>
+            <ol className={blogStyles.posts}>
                 {data.allMarkdownRemark.edges.map((edge) => {
                     return (
                         <li>
