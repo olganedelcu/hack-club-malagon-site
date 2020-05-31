7d373ab0957b6293d8cfc5040cf8954c918d6321
olganedelcu <olganedelcuam@gmail.com>
2020-05-31 13:19:31 +0200

    added a gatsby node API

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,3 +16,12 @@ module.exports.onCreateNode = ({ node, actions }) => {
     
     
 }
+
+module.exports.createPages = ({ graphql, actions }) => {
+    const { createPage } = actions
+    const blogTemplate = path.resolve('./src/templates/blog.js')
+    // 1. Gett path to template
+    // 2. Get markdown data
+    // 3. Create new pages
+
+}
