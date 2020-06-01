c17a8886e774d143d877ad3f6f44ff0d4992b01e
olganedelcu <olganedelcuam@gmail.com>
2020-06-01 23:07:57 +0200

    added more description

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,8 @@ const IndexPage = () => {
       <p>Nowadays learning to program is like getting a super power‍🚀: switched from consumer to creator.<br /> Suddenly, computers become a tool for creating.
       You have never had a better time to do it; Anywhere in the world, anyone with a laptop and wifi can learn to create an application. Building things has never been so globally democratized!</p>
       <h2>Objective and motivation</h2>
-    
+      <p>The goal of Hack Club is to help you become a hacker. "Hacker" is someone with the ability to solve problems, has ambition and wants to explore the world of knowledge.
+      </p>
     </Layout>
     </div>  
   )
