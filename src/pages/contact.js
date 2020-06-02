2f3cdc7d6ba33522f8adc1f5c931bc572997a9cd
olganedelcu <olganedelcuam@gmail.com>
2020-06-02 21:14:43 +0200

    new component

---
+++ src/pages/contact.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import Head from '../components/head'
+
+
+const ContactMe = () => {
+    return (
+        <div>
+            <Layout>
+                <Head title="Contact" />
+                <h1>Don't be shy and contact us!!</h1>
+            </Layout>
+        </div>
+    )
+}
+
+export default ContactMe
