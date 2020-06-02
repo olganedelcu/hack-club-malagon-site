73307b27d6ecf10d5efd3e159458559b937c9f7f
olganedelcu <olganedelcuam@gmail.com>
2020-06-02 20:22:44 +0200

    added footer items

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,9 +10,9 @@ const IndexPage = () => {
     <div>
       <Layout>
         <Head title="Home" />
-        <h1>What is <i>Hack Club😍</i> ?</h1>
+        <h1>What is <i>Hack Club</i>😍 ?</h1>
         <p>Hack Club is a global network of programming clubs where members learn to code through tinkering and building projects.</p>
-        <p>Link to webpage: <a className={styledIndex.a} href="https://hackclub.com" target="_blank">Hack Club</a></p>
+        <p>Link to webpage: <a className={styledIndex.a} href="https://hackclub.com" target="_blank">🔗Hack Club</a></p>
         <h2>✨What's Hack Club Malagón?</h2>
         <p>Nowadays learning to program is like getting a <b>super power‍</b>🚀: switched from consumer to creator.<br /> Suddenly, computers💻 become a tool for creating.
       You have never had a better time to do it; Anywhere in the world, anyone with a laptop and wifi can learn to create an application. Building things has never been so globally democratized!</p>
@@ -33,7 +33,7 @@ const IndexPage = () => {
         languages and sharing your projects to inspire others. Outside club meetings, you will
         find fliers promoting the club and club events that are open to all!<br />
         Be welcome to sing up!😍
-</p>
+      </p>
         <h2>🏫Sessions</h2>
         <p>The sessions last 1.5h.<br />In each students will share advances, technological knowledge, applications and future tech goals!Each student is provided with a computer so he/she can work freerly🤓</p>
       </Layout>
