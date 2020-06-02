da8405120aa1aa54651caa3b9472a2f206c8cae7
olganedelcu <olganedelcuam@gmail.com>
2020-06-02 20:14:24 +0200

    more info

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,18 +10,32 @@ const IndexPage = () => {
     <div>
       <Layout>
         <Head title="Home" />
-        <h1>What is Hack Club?</h1>
+        <h1>What is <i>Hack Club😍</i> ?</h1>
         <p>Hack Club is a global network of programming clubs where members learn to code through tinkering and building projects.</p>
         <p>Link to webpage: <a className={styledIndex.a} href="https://hackclub.com" target="_blank">Hack Club</a></p>
         <h2>✨What's Hack Club Malagón?</h2>
-        <p>Nowadays learning to program is like getting a super power‍🚀: switched from consumer to creator.<br /> Suddenly, computers💻 become a tool for creating.
+        <p>Nowadays learning to program is like getting a <b>super power‍</b>🚀: switched from consumer to creator.<br /> Suddenly, computers💻 become a tool for creating.
       You have never had a better time to do it; Anywhere in the world, anyone with a laptop and wifi can learn to create an application. Building things has never been so globally democratized!</p>
         <h2>🏆Objective and motivation</h2>
         <p>The goal of Hack Club is to help you become a <b>hacker👩‍💻</b>. <br />"Hacker" is someone with the ability to solve problems, has ambition and wants to explore the world of knowledge.
       </p>
-        <p><b>"We believe in a world where each young person has the power to be the change they want to see around them. At Hack Club Malagón, we want to make it happen"
-        </b></p>
-        <h2>What’s made inside Hack Clubs?</h2>
+        <p><i>"We believe in a world where each young person has the power to be the change they want to see around them. At Hack Club , we want to make it happen"
+        </i></p>
+        <h2>🎒What’s made inside Hack Clubs?</h2>
+        <p>Taking a look inside any Hack Club meeting, you will find people working on their
+        computers and  building their own projects (websites, games, etc.)🙈 🙉 🙊 . Some students will
+        be collaborating together, while others will be developing them independently. These
+        students are creating what they want to see in the world without any restrictions.<br /> The
+        environment inside this club is a friendly, inclusive, and productive one. Whether you’re
+        a first-time coder, or you are an expert, Hack Club welcomes students of all levels💯. If
+        you don’t know how to code but want to create something, this club is a very good place
+        to get started. If you’re an expert, you can grow your skills here by learning new coding
+        languages and sharing your projects to inspire others. Outside club meetings, you will
+        find fliers promoting the club and club events that are open to all!<br />
+        Be welcome to sing up!😍
+</p>
+        <h2>🏫Sessions</h2>
+        <p>The sessions last 1.5h.<br />In each students will share advances, technological knowledge, applications and future tech goals!Each student is provided with a computer so he/she can work freerly🤓</p>
       </Layout>
     </div>
   )
