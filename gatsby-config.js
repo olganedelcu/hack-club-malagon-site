e80a94b5f36f93bb207623fab1e36f65dc228c7d
olganedelcu <olganedelcuam@gmail.com>
2020-06-06 21:29:58 +0200

    bug

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,3 +39,43 @@ module.exports = {
   ]
 }
 
+module.exports = {
+  siteMetadata: {
+    title: 'Hack Club Malagón',
+    author:'HCM team'
+  },
+  plugins:[
+
+      'gatsby-plugin-react-helmet',
+      {
+        resolve: 'gatsby-source-contentful',
+        options: {
+          spaceId: process.env.CONTENTFUL_SPACE_ID,
+          accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+        }
+      },
+      'gatsby-plugin-sass',
+      {
+        resolve: 'gatsby-source-filesystem',
+        options: {
+          name: 'src',
+          path: `${__dirname}/src/`
+        }
+      },
+      'gatsby-plugin-sharp',
+      {
+        resolve: 'gatsby-transformer-remark',
+        options: {
+          plugins: [
+            'gatsby-remark-relative-images',{
+              resolve: 'gatsby-remark-images',
+              options: {
+                maxWidth: 750,
+                linkImagesToOriginal: false
+              }
+            }
+          ]
+        }
+      }
+  ]
+}
