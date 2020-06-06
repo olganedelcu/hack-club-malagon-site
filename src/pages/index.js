9ccb58ca6c957bf52f9f6ebb2fe524684e33eecb
olganedelcu <olganedelcuam@gmail.com>
2020-06-06 23:03:37 +0200

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const IndexPage = () => {
         <Head title="Home" />
         <h1>What is <b>Hack Club</b>😍 ?</h1>
         <p>Hack Club is a global network of programming clubs where members learn to code through tinkering and building projects.</p>
-        <p>Link to webpage: <a className={styledIndex.a} href="https://hackclub.com" target="_blank">🔗Hack Club</a></p>
+        <p>Link to webpage: <a className={styledIndex.a} href="https://hackclub.com" target="_blank" rel="noreferrer" >🔗Hack Club</a></p>
         <h2>✨What's Hack Club Malagón?</h2>
         <p>Nowadays learning to program is like getting a <b>super power‍</b>🚀: switched from consumer to creator.<br /> Suddenly, computers💻 become a tool for creating.
       You have never had a better time to do it; Anywhere in the world, anyone with a laptop and wifi can learn to create an application. Building things has never been so globally democratized!</p>
