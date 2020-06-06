db2670bb12c5050b84715165bb24c66782253f71
olganedelcu <olganedelcuam@gmail.com>
2020-06-06 22:44:16 +0200

    added fonts

--- src/components/header.js
+++ src/components/header.js
@@ -16,7 +16,7 @@ const Header = () => {
     `)
     return (
         <div className={headerStyles.header}>
-            <h1>
+            <h1 className={headerStyles.hack}>
                 <Link className={headerStyles.title} to="/">{data.site.siteMetadata.title}</Link>
             </h1>
             <nav>
