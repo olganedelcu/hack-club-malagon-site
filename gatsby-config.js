e9d4d1c1d6e616d56a1ef1ff7206629230f60a38
olganedelcu <olganedelcuam@gmail.com>
2020-06-07 19:39:37 +0200

    solved bug

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,29 +4,30 @@ module.exports = {
     author:'HCM team'
   },
   plugins:[
-      'gatsby-plugin-react-helmet',
+    `gatsby-plugin-react-helmet`,
       {
-        resolve: 'gatsby-source-contentful',
+        resolve: `gatsby-source-contentful`,
         options: {
           spaceId: process.env.CONTENTFUL_SPACE_ID,
           accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
         }
       },
-      'gatsby-plugin-sass',
+      `gatsby-plugin-sass`,
       {
-        resolve: 'gatsby-source-filesystem',
+        resolve: `gatsby-source-filesystem`,
         options: {
           name: 'src',
           path: `${__dirname}/src/`
         }
       },
-      'gatsby-plugin-sharp',
+      `gatsby-plugin-sharp`,
       {
-        resolve: 'gatsby-transformer-remark',
+        resolve: `gatsby-transformer-remark`,
         options: {
           plugins: [
-            'gatsby-remark-relative-images',{
-              resolve: 'gatsby-remark-images',
+            `gatsby-remark-relative-images`,
+            {
+              resolve: `gatsby-remark-images`,
               options: {
                 maxWidth: 750,
                 linkImagesToOriginal: false
